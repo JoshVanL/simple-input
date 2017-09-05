89af64e862ceef49a9ac1ac2085bd937630c7ea3
Joshua  Van Leeuwen <joshua.vanleeuwen@jetstack.io>
2017-09-05 19:31:17 +0100

    Moved pkg to seperate file

---
+++ simple-input.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+func Catch() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	fmt.Printf("\nHandle exit\n")
+
+	os.Exit(1)
+}
+
+func YesNo(query, promt string, defYes bool) (responce bool) {
+	go Catch()
+
+	reader := bufio.NewReader(os.Stdin)
+	var option string
+	if defYes {
+		option = " [Y/n]"
+	} else {
+		option = " [y/N]"
+	}
+
+	fmt.Printf("\n%s%s\n", query, option)
+
+	acc := false
+	for !acc {
+		fmt.Print(promt)
+
+		res, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		res = res[0 : len(res)-1]
+
+		res = strings.ToLower(res)
+		if res == "y" || res == "yes" {
+			responce = true
+			acc = true
+		} else if res == "n" || res == "no" {
+			responce = false
+			acc = true
+		} else if res == "" {
+			responce = defYes
+			acc = true
+		} else {
+			fmt.Printf("Bad responce. - %s", option)
+		}
+	}
+
+	return responce
+}
+
+func Select(query, promt string, choice []string) (responce string) {
+	go Catch()
+
+	reader := bufio.NewReader(os.Stdin)
+	var n int
+	acc := false
+	fmt.Printf("\n%s\n", query)
+	for i, s := range choice {
+		fmt.Printf("%d. %s\n", i+1, s)
+	}
+
+	for !acc {
+		fmt.Printf("%s", promt)
+
+		responce, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		responce = responce[0 : len(responce)-1]
+
+		n, err = strconv.Atoi(responce)
+		if err != nil || n < 1 || n > len(choice) {
+			fmt.Printf("Responce must be a number between 1 and %d\n", len(choice))
+		} else {
+			acc = true
+		}
+	}
+
+	responce = choice[n-1]
+
+	return responce
+}
+
+func Open(query, promt string) (responce string) {
+	go Catch()
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("\n%s\n", query)
+	acc := false
+
+	for !acc {
+		fmt.Print(promt)
+
+		res, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		res = res[0 : len(res)-1]
+
+		if res == "" {
+			fmt.Print("Nothing entered\n")
+		} else {
+			responce = res
+			acc = true
+		}
+	}
+
+	return responce
+}
